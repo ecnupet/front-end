c5384ead02baebb9773622f925e8e4182db29182
shanc <shanc@shancdeMacBook-Pro.local>
2021-03-10 19:37:31 +0800

    ci implements upload

---
+++ uploadoss.sh
@@ -0,0 +1,8 @@
+wget https://devtools.qiniu.com/qshell-v2.4.3-linux-amd64.tar.gz
+tar -zxvf qshell-v2.4.3-linux-amd64.tar.gz
+./qshell account $AK $SK 单超
+./qshell listbucket frontendecnupet
+./qshell listbucket frontendecnupet | awk '{print $1}' > list.txt
+./qshell batchdelete --force frontendecnupet -i list.txt
+ls -al
+./qshell qupload2 --src-dir=build --bucket=frontendecnupet
