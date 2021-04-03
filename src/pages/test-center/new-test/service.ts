47d694d6472134bb1485df605be079d410707972
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2021-04-04 00:54:22 +0800

    chore: 添加生成试卷时的交互文案

--- src/pages/test-center/new-test/service.ts
+++ src/pages/test-center/new-test/service.ts
@@ -1,6 +1,6 @@
 import { makeAutoObservable } from "mobx";
 import { QuestionType } from "../../../models";
-import { BackendServiceFactory } from "../../../services";
+import { BackendServiceFactory, InteractFactory } from "../../../services";
 import { globalStore } from "../../../store";
 import { openPage, withType } from "../../../utils/common";
 import { TakeTestParameters } from "../../take-test";
@@ -25,12 +25,14 @@ export class NewTestService {
 
   async handleCreateNewTest(form: NewTestForm) {
     const username = globalStore.user.userName;
+    InteractFactory.getMessager().info("系统正在生成试卷，请稍等");
     const {
       data: { questionId, quizId },
     } = await BackendServiceFactory.getQuizService().newQuiz({
       types: form.types,
       userName: username,
     });
+    InteractFactory.getMessager().success("试卷已生成，请前往新页面答题！");
     openPage(
       "/take-test",
       withType<TakeTestParameters>({ questionIds: questionId, quizId })
